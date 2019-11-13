e9ee7c4b64daafe1af589888d10936c6b6979321
idei0 <idei000sports@gmail.com>
2019-11-13 17:47:31 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@
   get 'input_performer/show' => 'input_performer/show#show'
   get 'input_performer/new'
   resources :users
+  root 'today#index'
 
   get 'edit_event/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

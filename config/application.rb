503eae30ee17bb7aac3c7b5882a292cd781c9642
idei0 <idei000sports@gmail.com>
2019-11-13 17:43:34 +0900

    first commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 module Unko
   class Application < Rails::Application
@@ -16,5 +15,6 @@ module Unko
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile = false
   end
 end

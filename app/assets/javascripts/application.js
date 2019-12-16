b937fbf5ee8eb79f64795f562b5c0296d47ebf2f
idei0 <idei000sports@gmail.com>
2019-12-16 09:51:43 +0900

    PWA2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,6 +40,14 @@ $(function() {
     });
 });
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered!');
+    });
+}
+
+
 /*
 $(function () {
     function eventCalendar() {

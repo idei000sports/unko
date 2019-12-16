4d3eee598af6d875bfcc3348c269de34ad70ab24
idei0 <idei000sports@gmail.com>
2019-12-16 09:47:18 +0900

    PWA化

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,21 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+{
+  "name":"スケジュール",
+  "short_name":スケジュール",
+  "icons": [{
+      "src": "icon.png",
+      "sizes":"192x192",
+      "type": "image/png"
+    }, {
+      "src": "icon.png",
+      "sizes":"512x512",
+      "type": "image/png"
+    }],
+  "start_url": "/index.html?utm_source=homescreen",
+  "display": "standalone",
+  "background_color": "#FFFFFF",
+  "theme_color": "#FFFFFF"
+}

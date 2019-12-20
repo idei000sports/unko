fbde859f05b509efb8e7c6668f905ac4934bf19f
idei0 <idei000sports@gmail.com>
2019-12-20 15:11:14 +0900

    微調整につぐ微調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,3 +39,12 @@ $(function() {
 
     });
 });
+
+$(document).ready(function(){
+    $('#topBtn').click(function () { // #topBtnをクリックすると
+        $('body,html').animate({ // いちばん上にanimateする
+        scrollTop: 0 // 戻る位置
+        }, 400); // 戻るスピード
+        return false;
+    });
+});

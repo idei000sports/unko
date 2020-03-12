8a6c7b8d9a324b652d35bf9d37d191d932e41bc5
idei0 <idei000sports@gmail.com>
2020-03-12 15:05:35 +0900

    ユーザー名の取得方式変更

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,13 +1,9 @@
 class AccountsController < ApplicationController
 	def index
-		@user = User.find_by(id: current_user.id)
-		
-		#@createds =Event.where(user_id: current_user.id)
 		@createds =Event.includes(:prefecture).where(user_id: current_user.id)
 	end
 
 	def mylist
-		@user = User.find_by(id: current_user.id)
 		@likes = Like.where(user_id: current_user.id).all	
 	end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
 
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_current_user
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :set_current_user
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
-  end
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+	end
 
-  def set_current_user
-	@current_user = User.find_by(id: session[:user_id])
-  end
+	def set_current_user
+		@current_user = User.find_by(id: session[:user_id])
+	end
 
 end

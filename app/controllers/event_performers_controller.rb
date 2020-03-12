dbf40c0411ee88fe2795d7b7c2341e705ad9d95c
idei0 <idei000sports@gmail.com>
2020-03-12 15:09:26 +0900

    半角スペースをtabに

--- app/controllers/event_performers_controller.rb
+++ app/controllers/event_performers_controller.rb
@@ -1,5 +1,5 @@
 ﻿class EventPerformersController < ApplicationController
-  before_action :set_event_performer, only: [:destroy]
+	before_action :set_event_performer, only: [:destroy]
 
 	#出演者からイベントを絞り込むときのメソッド
 	def select
@@ -10,40 +10,40 @@
 
 
 
-  def input
-	@event = Event.find(params[:id])
-	@event_performers = EventPerformer.where("event_id = ?" , @event.id)
-	@event_performer = EventPerformer.new
-  end
+	def input
+		@event = Event.find(params[:id])
+		@event_performers = EventPerformer.where("event_id = ?" , @event.id)
+		@event_performer = EventPerformer.new
+	end
 
-  def create
-	@event_performer = EventPerformer.new(event_performer_params)
-	@event_id = @event_performer.event_id
+	def create
+		@event_performer = EventPerformer.new(event_performer_params)
+		@event_id = @event_performer.event_id
 	
-	@event_performer.save
+		@event_performer.save
 
-	redirect_to controller:'event_performers', action: 'input', id: @event_id
-  end
+		redirect_to controller:'event_performers', action: 'input', id: @event_id
+	end
 
 
-  def destroy
-	@event_id = @event_performer.event_id
-	@event_performer.destroy
+	def destroy
+		@event_id = @event_performer.event_id
+		@event_performer.destroy
 
-	redirect_to controller:'event_performers', action: 'input', id: @event_id
-  end
+		redirect_to controller:'event_performers', action: 'input', id: @event_id
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    #コールバックを使用して、アクション間で共通のセットアップまたは制約を共有します。
-    def set_event_performer
-      @event_performer = EventPerformer.find(params[:id])
-    end
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	#コールバックを使用して、アクション間で共通のセットアップまたは制約を共有します。
+	def set_event_performer
+		@event_performer = EventPerformer.find(params[:id])
+	end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    #恐ろしいインターネットからのパラメーターを決して信用せず、ホワイトリストのみを許可します。
-    def event_performer_params
-      params.require(:event_performer).permit(:event_id, :performer_name)
-    end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	#恐ろしいインターネットからのパラメーターを決して信用せず、ホワイトリストのみを許可します。
+	def event_performer_params
+		params.require(:event_performer).permit(:event_id, :performer_name)
+	end
 
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,16 @@
 class LikesController < ApplicationController
-  def create
-	  like = current_user.likes.build(event_id: params[:event_id])
-	  like.save
+	def create
+		like = current_user.likes.build(event_id: params[:event_id])
+		like.save
 
-	event_id = params[:event_id]
-	redirect_to '/events/' + event_id
-  end
+		event_id = params[:event_id]
+		redirect_to '/events/' + event_id
+	end
 
-  def destroy
-	  like = Like.find_by(event_id: params[:event_id], user_id: current_user.id)
-	  like.destroy
-	event_id = params[:event_id]
-	  redirect_to '/events/' + event_id
-  end
+	def destroy
+		like = Like.find_by(event_id: params[:event_id], user_id: current_user.id)
+		like.destroy
+		event_id = params[:event_id]
+		redirect_to '/events/' + event_id
+	end
 end
